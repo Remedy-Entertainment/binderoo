331df2d37012008863c0b083d24778a2c004177e
GooberMan <gooberman@gmail.com>
2016-09-28 23:31:53 +0300

    fastdelegate included in the core include

--- binderoo_common/cpp/interface/binderoo/defs.h
+++ binderoo_common/cpp/interface/binderoo/defs.h
@@ -34,6 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //----------------------------------------------------------------------------
 
 #include <cstdint>
+#include "fastdelegate/fastdelegate.h"
 
 #if defined( BINDEROO_EXPORT )
 	#define BIND_DLL			__declspec( dllexport )
